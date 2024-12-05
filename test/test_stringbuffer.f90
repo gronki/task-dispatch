ffca73375ca94fa96ae79bbb05c25b8b782e51da
Dominik Gronkiewicz <gronki@gmail.com>
2024-12-05 01:42:44 +0100

    it is all worth nothing anyway

--- test/test_stringbuffer.f90
+++ test/test_stringbuffer.f90
@@ -5,26 +5,26 @@ program test_stringbuffer
     implicit none (type, external)
 
     type(stringbuffer_t) :: sb
+    character(len=:), allocatable :: s
 
     print *, '*** TEST_STRINGBUFFER ***'
 
     call sb%append("a")
-    associate (s => sb%str())
-        if (len(s) /= 1) error stop "len(s) /= 1"
-        if (s /= "a") then
-            write (error_unit, *) "'", s, "' /= a"
-            error stop
-        end if
-    end associate
+    s = sb % str()
+
+    if (len(s) /= 1) error stop "len(s) /= 1"
+    if (s /= "a") then
+        write (error_unit, *) "'", s, "' /= a"
+        error stop
+    end if
 
     call sb%append("bc")
-    associate (s => sb%str())
-        if (len(s) /= 3) error stop "len(s) /= 3"
-        if (s /= "abc") then
-            write (error_unit, *) "'", s, "' /= abc"
-            error stop
-        end if
-    end associate
+    s = sb % str()
 
+    if (len(s) /= 3) error stop "len(s) /= 3"
+    if (s /= "abc") then
+        write (error_unit, *) "'", s, "' /= abc"
+        error stop
+    end if
 
 end program
